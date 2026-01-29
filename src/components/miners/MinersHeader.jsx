c7badcf499292d4809239866350ac1e87c595a82
clawlynx <clawlynx77@gmail.com>
2026-01-29 16:13:25 +0530

    added the new model

--- src/components/miners/MinersHeader.jsx
+++ src/components/miners/MinersHeader.jsx
@@ -4,18 +4,25 @@ import AddMinerModal from "./AddMinerModal";
 export default function MinersHeader() {
   const [addForm, setAddForm] = useState(false);
   return (
-    <div className="p-6 border-b border-[#DCDCDC] flex items-center justify-between">
+    <div className="p-6 border-b border-[#DCDCDC] flex md:flex-row flex-col md:items-center justify-between gap-3">
       <div>
         <h1 className="text-2xl font-semibold text-black">Miners</h1>
-        <p className="text-md text-gray-500">Manage all mining equipment and monitor performance</p>
+        <p className="text-md text-gray-500">
+          Manage all mining equipment and monitor performance
+        </p>
       </div>
+      <div className="flex gap-2">
+        <button className="bg-[#3893D0] hover:bg-[#2c7cb5] text-white rounded-xl px-4 py-1 transition-all">
+          + Add Miner Model
+        </button>
 
-      <button
-        onClick={() => setAddForm(true)}
-        className="bg-[#3893D0] hover:bg-[#2c7cb5] text-white rounded-xl px-4 py-1 transition-all"
-      >
-        + Add Miner
-      </button>
+        <button
+          onClick={() => setAddForm(true)}
+          className="bg-[#3893D0] hover:bg-[#2c7cb5] text-white rounded-xl px-4 py-1 transition-all"
+        >
+          + Add Miner
+        </button>
+      </div>
 
       {addForm && <AddMinerModal onClose={() => setAddForm(false)} />}
     </div>
