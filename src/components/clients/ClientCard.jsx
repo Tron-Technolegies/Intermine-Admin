f9b084b37dfe262d9179e920c16f104bf6b343d7
clawlynx <clawlynx77@gmail.com>
2026-04-22 18:27:06 +0530

    added new way of adding clients, and miner model popup

--- src/components/clients/ClientCard.jsx
+++ src/components/clients/ClientCard.jsx
@@ -30,7 +30,10 @@ export default function ClientCard({ client, onViewDetails }) {
       <div className="flex gap-5 md:flex-row flex-col md:items-center justify-between">
         <div>
           <h3 className="text-lg font-semibold text-gray-900">
-            {client.clientName}
+            {client.clientName}{" "}
+            <span className="text-xs font-normal ml-5">
+              ({client.clientType?.toUpperCase()})
+            </span>
           </h3>
           <p className="text-sm text-gray-600">{client.clientId}</p>
         </div>
